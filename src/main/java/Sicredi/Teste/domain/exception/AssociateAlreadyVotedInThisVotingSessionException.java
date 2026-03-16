8f1be4453f5e366b8354628170d370341375631b
Jeferson Bitencourt <jeferson267@hotmail.com>
2026-03-16 16:29:09 -0300

    main/feat: change log

--- src/main/java/Sicredi/Teste/domain/exception/AssociateAlreadyVotedInThisVotingSessionException.java
+++ src/main/java/Sicredi/Teste/domain/exception/AssociateAlreadyVotedInThisVotingSessionException.java
@@ -2,6 +2,6 @@ package Sicredi.Teste.domain.exception;
 
 public class AssociateAlreadyVotedInThisVotingSessionException extends RuntimeException{
     public AssociateAlreadyVotedInThisVotingSessionException(Long votingSession, String associateId) {
-        super("Associate with id " + associateId.substring(associateId.length() - 4) + " already voted in this voting session with id " + votingSession);
+        super("Associate with id finished in " + associateId.substring(associateId.length() - 4) + " already voted in this voting session with id " + votingSession);
     }
 }
